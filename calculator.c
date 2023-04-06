2389c7a5aa7a26e0e90021621d6ac9d65dc71bfe
ProgrammingwDani <129200430+ProgrammingwDani@users.noreply.github.com>
2023-04-06 23:56:32 +0200

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,52 +1,98 @@
 #include <stdio.h>
 #include <math.h>
-int main
+
+double addition(int num1, int num2)
+{
+    double result = num1 + num2;
+    printf("The addition of %d and %d is %.1lf\n", num1, num2, result);
+    return result;
+}
+
+double subtraction(int num1, int num2)
+{
+    double result = num1 - num2;
+    printf("The subtraction of %d and %d is %.1lf\n", num1, num2, result);
+    return result;
+}
+
+double multiplication(int num1, int num2)
+{
+    double result = num1 * num2;
+    printf("The multiplication of %d and %d is %.1lf\n", num1, num2, result);
+    return result;
+}
+
+double division(int num1, int num2)
+{
+    if (num2 == 0)
+    {
+        printf("Error, cannot divide by 0\n");
+        return 0;
+    }
+    else
+    {
+        double result = (double) num1 / num2;
+        printf("The division of %d and %d is %.1lf\n", num1, num2, result);
+        return result;
+    }
+}
+
+double power(int num1, int num2)
+{
+    double result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    printf("%d to the power %d is %.1lf\n", num1, num2, result);
+    return result;
+}
+
+double square_root(int num1)
+{
+    double result = sqrt(num1);
+    printf("The square root of %d is %.1lf\n", num1, result);
+    return result;
+}
+
+int main()
 {
     int num1, num2;
     char c;
-    double result, sum = 0;
+    double result;
+
     scanf("%d %c", &num1, &c);
-    if (c == '+')
+
+    switch (c)
     {
-        scanf("%d", &num2);
-        result = num1 + num2;
-        printf("The addition of %d and %d is %,1lf", num1, num2, result);
+        case '+':
+            scanf("%d", &num2);
+            result = addition(num1, num2);
+            break;
+        case '-':
+            scanf("%d", &num2);
+            result = subtraction(num1, num2);
+            break;
+        case '*':
+            scanf("%d", &num2);
+            result = multiplication(num1, num2);
+            break;
+        case '/':
+            scanf("%d", &num2);
+            result = division(num1, num2);
+            break;
+        case '^':
+            scanf("%d", &num2);
+            result = power(num1, num2);
+            break;
+        case 's':
+            result = square_root(num1);
+            break;
+        default:
+            printf("Invalid operation\n");
+            result = 0;
+            break;
     }
-    else if (c == '-')
-    {
-        scanf("%d", &num2);
-        result = num1 - num2;
-        printf("The substraction of %d and %d is %.1lf", num1, num2, result);
-    }
-    else if (c == '*')
-    {
-        scanf("%d", &num2);
-        result = num1 / num2;
-        printf("The multiplication of %d and %d is %lf", num1, num2, result);
-    }
-    else if (c == '/')
-    {
-        scanf("%d", &num2);
-        if (num2 == 0)
-        {
-            printf("Error, cannot divide by 0");
-        }
-        else
-            result = num1 / num2;
-        printf("The division of %d and %d is %lf", num1, num2, result);
-    }
-    else if (c == '^')
-    {
-        for (int i = 1; i <= num; i++)
-        {
-            sum = sum + num1;
-        }
-        printf("%d to the power %d is %lf", num1, num2, result);
-    }
-    else if (c == 's')
-    {
-        result = sqrt(num1);
-        printf("The square of %d is %lf", num1, result);
-    }
+
     return 0;
 }
